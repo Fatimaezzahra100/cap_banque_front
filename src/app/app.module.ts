aa2e356b05979a10657dbdcedb022fb03f1659f4
Fatima ELGHANEMY <fatimaezzahra.elg@gmail.com>
2022-01-12 15:52:08 +0100

    création des components compte

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './component/home/home.component';
-import { NavbarComponent } from './component/navbar/navbar.component';
+import { CompteAddComponent } from './MainPage/component/compte/compte-add/compte-add.component';
+import { CompteConsultComponent } from './MainPage/component/compte/compte-consult/compte-consult.component';
+import { CompteEditComponent } from './MainPage/component/compte/compte-edit/compte-edit.component';
+import { HomeComponent } from './MainPage/component/home/home.component';
+import { NavbarComponent } from './MainPage/component/navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    CompteConsultComponent,
+    CompteAddComponent,
+    CompteEditComponent
+   
   ],
   imports: [
     BrowserModule,
